50be12e2382ea7bc37048c55763b9b02e94d47bb
Olafur Arason <olafura@olafura.com>
2018-09-24 22:04:05 -0400

    Translate notes to jsx

--- lib/components/notes.ex
+++ lib/components/notes.ex
@@ -1,8 +1,9 @@
 defmodule MyScenicApp.Component.Notes do
   use Scenic.Component
+  require ScenicJsx
+  import ScenicJsx
 
   alias Scenic.ViewPort
-  alias Scenic.Graph
 
   import Scenic.Primitives, only: [{:text, 3}, {:rect, 3}]
 
@@ -23,10 +24,12 @@ defmodule MyScenicApp.Component.Notes do
       opts[:viewport]
       |> ViewPort.info()
 
-    graph =
-      Graph.build(font_size: @font_size, translate: {0, vp_height - @height})
-      |> rect({vp_width, @height}, fill: {48, 48, 48})
-      |> text(notes, translate: {@indent, @font_size * 2})
+    graph = ~z(
+        <font_size=#{@font_size} translate=#{{0, vp_height - @height}}>
+          <rect fill=#{{48, 48, 48}}>#{{vp_width, @height}}</rect>
+          <text translate=#{{@indent, @font_size * 2}}>#{notes}</text>
+        </>
+      )
       |> push_graph()
 
     {:ok, %{graph: graph, viewport: opts[:viewport]}}
